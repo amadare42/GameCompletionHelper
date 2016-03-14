c42eb7bd12e5303ed3c9131759b9c32780e55b05
Amadare42 <amadare42@gmail.com>
2016-03-14 21:41:22 +0200

    Added missing formatters.

---
+++ GameCompletionHelper/Formatters/DateTimeFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace GameCompletionHelper.Formatters
+{
+    static class DateTimeFormatter
+    {
+        public static string ConvertToString(DateTime time, bool includeAgo = false)
+        {
+            if (time == default(DateTime))
+                return "Never";
+
+            StringBuilder builder = new StringBuilder($"{time.ToString()}");
+            if (includeAgo)
+            {
+                var ago = TimeSpanFormatter.ConvertToString(DateTime.Now - time, TimeSpanFormatter.FormatOptions.OmmitSeconds);
+                builder.Append($" ({ago} ago)");
+            }
+            return builder.ToString();
+        }
+
+        [Flags]
+        public enum DateTimeFormatterOptions
+        {
+            None,
+            IncludeAgo
+        }
+    }
+}
---
+++ GameCompletionHelper/Formatters/TimeSpanFormatter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCompletionHelper.Formatters
+{
+    static class TimeSpanFormatter
+    {
+        private const string DefaultFormat = "{0} hour{1} {2} minute{3} {4} second{5}";
+        private const string OmmitedSecondsFormat = "{0} hour{1} {2} minute{3}";
+
+        public static string ConvertToString(TimeSpan span, FormatOptions options = FormatOptions.None)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (span.Days > 0)
+                builder.AppendFormat("{0} day{1} ", span.Days, GetS(span.Days));
+            builder.AppendFormat(
+                options.HasFlag(FormatOptions.OmmitSeconds)
+                    ? OmmitedSecondsFormat
+                    : DefaultFormat,
+                span.Hours, GetS(span.Hours),
+                span.Minutes, GetS(span.Minutes),
+                span.Seconds, GetS(span.Seconds)
+                );
+
+            return builder.ToString();
+        }
+
+        private static string GetS(int value)
+        {
+            return value != 1 ? "s" : string.Empty;
+        }
+
+        [Flags]
+        public enum FormatOptions
+        {
+            None,
+            OmmitSeconds
+        }
+    }
+}
